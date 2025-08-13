25a7b8bc54d487a1692844711db45eb5e5a82fe5
Emillock <mirzazadeemil2005@gmail.com>
2025-08-13 15:19:49 +0400

    upd fe1 predictions

--- src/models/predict_model_fe1.py
+++ src/models/predict_model_fe1.py
@@ -1,43 +1,36 @@
 import pickle
-import re
 
-import numpy as np
 import pandas as pd
-from category_encoders import CatBoostEncoder
-from scipy.stats.mstats import winsorize
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
 
 
 def main():
-    df = pd.read_parquet('./data/processed/multisim_dataset.parquet')
+    df = pd.read_parquet('./data/processed/data_usage_production.parquet')
 
     target = 'data_compl_usg_local_m1'
     X = df.drop(columns=[target])
     y = df[target]
 
-    X_train, y_train, X_test, y_test = train_test_split(
+    X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    rf = RandomForestRegressor(
-        n_estimators=100, 
-        max_depth=10, 
-        random_state=42, 
-        n_jobs=-1, 
-        random_state=42
-    )
+    filename = './models/rf_fe1.pkl'
 
-    rf.fit(X_train, y_train)
-    filename = 'my_model.pkl'
-    with open(filename, 'wb') as file:
-        pickle.dump(rf, file)
+    with open(filename, "rb") as f:
+        rf : RandomForestRegressor = pickle.load(f)
 
-    print("Load model and run predictions")
+    y_pred=rf.predict(X_test)
+    
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f"MAE: {mae:.3f}")
+    print(f"MSE: {mse:.3f}")
+    print(f"R² Score: {r2:.3f}")
 
 
 if __name__ == "__main__":

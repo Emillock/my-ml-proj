d82abb439e25a9d93a658f886a1bd879d008f389
Emillock <mirzazadeemil2005@gmail.com>
2025-08-13 16:30:09 +0400

    upd trainmodel prediction and training

--- src/models/predict_model_fe2.py
+++ src/models/predict_model_fe2.py
@@ -1,5 +1,26 @@
+import pickle
+
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score, train_test_split
+from xgboost import XGBClassifier
+
 def main():
-    print("Load model and run predictions")
+    df = pd.read_parquet('./data/processed/multisim_dataset.parquet')
+
+    target = 'target'
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    filename = './models/xgb_trainmodel.pkl'
+
+    with open(filename, "rb") as f:
+        xgb : XGBClassifier = pickle.load(f)
+    
+    cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=0)
+    print("Precision:", cross_val_score(xgb, X, y, cv=cv, scoring="precision").mean())
+    print("Recall:", cross_val_score(xgb, X, y, cv=cv, scoring="recall").mean())
+    print("F1 Score:", cross_val_score(xgb, X, y, cv=cv, scoring="f1").mean())
 
 
 if __name__ == "__main__":
--- src/models/train_model_trainmodel.py
+++ src/models/train_model_trainmodel.py
@@ -1,5 +1,44 @@
+import pickle
+import re
+import os
+import json
+
+import joblib
+import numpy as np
+import pandas as pd
+from category_encoders import CatBoostEncoder
+from scipy.stats.mstats import winsorize
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from xgboost import XGBClassifier
+
+
 def main():
-    print("Train your model")
+    df = pd.read_parquet('./data/processed/multisim_dataset.parquet')
+
+    target = 'target'
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    path = "./data/interim/multisim_weights.json"   # change to your file
+    with open(path, "r", encoding="utf-8") as file:
+        weights = json.load(file)
+    params = {'n_estimators': 186, 'max_depth': 10, 'learning_rate': 0.01155364929483116, 'subsample': 0.5596769098694525, 'colsample_bytree': 0.9364342412798315, 'min_child_weight': 7}
+    xgb = XGBClassifier(**params, feature_weights=weights)
+    print("X_train shape:", X_train.shape)
+    print("y_train shape:", y_train.shape)
+    xgb.fit(X_train, y_train)
+    filename = './models/xgb_trainmodel.pkl'
+
+    with open(filename, 'wb') as file:
+        pickle.dump(xgb, file)
 
 
 if __name__ == "__main__":
